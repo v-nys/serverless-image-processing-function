21814fcdd9b7cddfd6461c81e0ddbaad12daedf8
Vincent Nys <vincentnys@gmail.com>
2024-05-11 11:55:14 +0200

    Added basic shared secret authentication.

--- background-removal/handler.js
+++ background-removal/handler.js
@@ -1,12 +1,22 @@
 'use strict'
 
-module.exports = async (event, context) => {
-  const result = {
-    'body': JSON.stringify(event.body),
-    'content-type': event.headers["content-type"]
-  }
+const VALID_SECRET = 'my-secret-pw';
 
-  return context
-    .status(200)
-    .succeed(result)
+module.exports = async (event, context) => {
+
+  console.debug(event.headers);
+
+  const auth_header = event.headers.authorization;
+  if (auth_header && auth_header.startsWith("Bearer ")) {
+        const token = auth_header.substring(7);
+        if (token.trim() === VALID_SECRET) {
+            return context.status(200).succeed("Token was valid");
+        } else {
+            return context.status(403).fail("Invalid token");
+        }
+    }
+    else {
+        return context.status(403).fail("Token missing or not correctly formatted");
+    }  
+
 }

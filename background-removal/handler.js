aebb0b2f972d151d89eb28300c8b56624397dd48
Vincent Nys <vincentnys@gmail.com>
2024-05-11 20:32:55 +0200

    Added this to show function can be tested without going through full deployment. Setting the env variable when running npm test overrides the function's value. Alternatively, an env variable could be set for the whole faasd installation (but that would involve messing with existing installations and this is a good way to show a feature).

--- background-removal/handler.js
+++ background-removal/handler.js
@@ -2,6 +2,11 @@
 
 const { readFile } = require('fs').promises;
 const jimp = require('jimp');
+const node_env = process.env.NODE_ENV;
+if (!node_env || node_env !== "test" && node_env !== "production") {
+    console.log("Node environment was not correctly defined.");
+    process.exit(1);
+}
 
 module.exports = async (event, context) => {
     const auth_header = event.headers.authorization;
@@ -11,7 +16,8 @@ module.exports = async (event, context) => {
     const reconverted = writable_to_file.toString('base64');
 
     if (auth_header && auth_header.startsWith("Bearer ")) {
-        const expected_token = await readFile("/var/openfaas/secrets/auth-token");
+
+        const expected_token = node_env === "test" ? "my-secret-pw" : await readFile("/var/openfaas/secrets/auth-token");
         const token = auth_header.substring(7);
         if (token.trim() === expected_token.toString().trim()) {
             return context.status(200).succeed(reconverted);
